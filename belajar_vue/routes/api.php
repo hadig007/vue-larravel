c8bf468bd580fbb50e0be05053a024333dd82583
hadig007 <62656877+hadig007@users.noreply.github.com>
2021-08-31 10:08:34 +0700

    menambah controller

--- belajar_vue/routes/api.php
+++ belajar_vue/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
-
+use App\Http\Controllers\Api\PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,9 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('posts', function (Request $request) {
-    return Post::all();
-});
-Route::get('posts/{id}', function (Request $request, $abc) {
-    return Post::findOrFail($abc);
-});
+// Route::get('posts', function (Request $request) {
+//     return Post::all();
+// });
+// Route::get('posts/{id}', function (Request $request, $abc) {
+//     return Post::findOrFail($abc);
+// });
+
+Route::get('posts',[PostController::class,'index']);
+Route::get('posts/{id}',[PostController::class,'show']);
